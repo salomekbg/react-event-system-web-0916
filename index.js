aa279c49c62822e049bb9ce015b093f9f86a84d0
SluvsP <salomekbg@gmail.com>
2016-11-17 17:24:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-const app = "I don't do much.";
+const React = require('react');
+
+class Tickler extends React.Component {
+  constructor() {
+    super();
+
+    this.tickle = this.tickle.bind(this);
+  }
+
+  tickle() {
+    console.log('Tee hee!');
+  }
+
+  render() {
+    return (
+      hi
+      // <button onClick={this.tickle}>Tickle me!</button>
+    )
+  }
+}
+
+module.exports = Tickler;
